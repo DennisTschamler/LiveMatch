64b38384426f4c2d76421adb8c320b9c01ae74b3
Invicz <dennist@seznam.cz>
2016-06-10 19:59:23 +0200

    init commit

---
+++ LiveMatch/src/api/dto/LeagueEntryAPI.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package api.dto;
+
+/**
+ *
+ * @author Dennis
+ */
+public class LeagueEntryAPI {
+    private String division;
+    private boolean isFreshBlood;
+    private boolean isHotStreak;
+    private boolean isInactive;
+    private boolean isVeteran;
+    private int leaguePoints;
+    private int losses;
+    private MiniSeriesAPI miniSeries;
+    private String playerOrTeamId;
+    private String playerOrTeamName;
+    private int wins;
+}
---
+++ LiveMatch/src/api/dto/ObserverAPI.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package api.dto;
+
+/**
+ *
+ * @author Dennis
+ */
+public class ObserverAPI {
+    private String encryptionKey;
+
+    public String getEncryptionKey() {
+        return encryptionKey;
+    }
+    
+    
+}
