d70163a58e1a002f2bdd106a926bbe66ffc8a456
Invicz <dennist@seznam.cz>
2016-06-10 20:08:07 +0200

    init commit

---
+++ LiveMatch/src/Utils/ImageUtils.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utils;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author Dennis
+ */
+public class ImageUtils {
+
+    public static Image resizeImage(Image image, int w, int h) {
+        BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = resizedImg.createGraphics();
+
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.drawImage(image, 0, 0, w, h, null);
+        g2.dispose();
+
+        return resizedImg;
+    }
+}
